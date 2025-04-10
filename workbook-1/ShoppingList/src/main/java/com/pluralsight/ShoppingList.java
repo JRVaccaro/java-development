446c8a78a42e0b6c876d605e00383c7e6f148ed2
JRVaccaro <ooliebud@gmail.com>
2025-04-10 09:27:59 -0400

    Inital Commit

---
+++ workbook-1/ShoppingList/src/main/java/com/pluralsight/ShoppingList.java
@@ -0,0 +1,16 @@
+package com.pluralsight;
+
+public class ShoppingList {
+    public static void main(String[] args) {
+        System.out.println("Grapes");
+        System.out.println("Blueberries");
+        System.out.println("Bananas");
+        System.out.println("Apples");
+        System.out.println("Mangos");
+        System.out.println("Raspberries");
+        System.out.println("Milk");
+        System.out.println("French Fries");
+        System.out.println("Bagels");
+        System.out.println("Cheese");
+    }
+}
