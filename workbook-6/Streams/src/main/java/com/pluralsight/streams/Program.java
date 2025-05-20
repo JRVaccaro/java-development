c1dc91e55628c316a7af397f9e2f427044f1a522
JRVaccaro <ooliebud@gmail.com>
2025-05-20 09:59:44 -0400

    Added list of 10 person objects to Program class

--- workbook-6/Streams/src/main/java/com/pluralsight/streams/Program.java
+++ workbook-6/Streams/src/main/java/com/pluralsight/streams/Program.java
@@ -1,7 +1,23 @@
 package com.pluralsight.streams;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Program {
     public static void main(String[] args) {
 
+        List<Person> people = new ArrayList<>();
+
+
+        people.add(new Person("Alice", "Wonder",25));
+        people.add(new Person("Bob", "Newt",30));
+        people.add(new Person("Charlie", "Felitn",22));
+        people.add(new Person("Diana", "Stone",28));
+        people.add(new Person("Ethan", "Slade", 35));
+        people.add(new Person("Fiona", "Yonu", 27));
+        people.add(new Person("George", "Tree",31));
+        people.add(new Person("Hannah", "Hopp",24));
+        people.add(new Person("Ian", "Chuck",29));
+        people.add(new Person("Jasmine", "Williams", 30));
     }
 }
