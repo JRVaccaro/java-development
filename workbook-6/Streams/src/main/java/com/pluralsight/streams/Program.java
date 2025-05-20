bef564299701aadb1eb783e746ce9c4568561478
JRVaccaro <ooliebud@gmail.com>
2025-05-20 10:12:58 -0400

    Added nameSearch method to search Person list by first or last name to Program class

--- workbook-6/Streams/src/main/java/com/pluralsight/streams/Program.java
+++ workbook-6/Streams/src/main/java/com/pluralsight/streams/Program.java
@@ -2,6 +2,7 @@ package com.pluralsight.streams;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Program {
     public static void main(String[] args) {
@@ -19,5 +20,25 @@ public class Program {
         people.add(new Person("Hannah", "Hopp",24));
         people.add(new Person("Ian", "Chuck",29));
         people.add(new Person("Jasmine", "Williams", 30));
+
+        nameSearch(people);
+    }
+    public static void nameSearch(List<Person> people){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please enter first or last name: ");
+        String userInput = scanner.nextLine().trim();
+
+        boolean found = false;
+
+        for (Person person : people){
+            if (person.getFirstName().equalsIgnoreCase(userInput) || person.getLastName().equalsIgnoreCase(userInput)){
+                System.out.println(person.getFirstName() + person.getLastName() + person.getAge());
+                found = true;
+            }
+        }
+        if (!found){
+            System.out.println("No match found.");
+        }
     }
 }
