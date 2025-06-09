a7288e44fbd8b52ce72b9dcc9271f2af4b55928a
JRVaccaro <ooliebud@gmail.com>
2025-06-09 14:51:13 -0400

    Worked on Northwind Products Exercise

---
+++ workbook-8/NorthwindProducts/src/main/java/com/pluralsight/Main.java
@@ -0,0 +1,29 @@
+package com.pluralsight;
+
+import java.sql.*;
+
+public class Main {
+    public static void main(String[] args) throws ClassNotFoundException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+
+        Connection connection = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/northwind",
+                "root",
+                "yearup"
+        );
+        Statement statement = connection.createStatement();
+
+        String query = "SELECT ProductName FROM products";
+
+        ResultSet results = statement.executeQuery(query);
+
+        while (results.next()){
+            String productName = results.getString("ProductName");
+            System.out.println(productName);
+
+        }
+        connection.close();
+
+
+    }
+}
